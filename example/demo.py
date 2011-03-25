c03303ce3bc156d87ad90744b555857e64405b07
Guobao Shen <devnull@localhost>
2011-03-25 11:43:06 -0400

    Add an example how to add/delete tag

--- example/demo.py
+++ example/demo.py
@@ -24,15 +24,49 @@ Created on Mar 14, 2011
 """
 
 from ChannelFinderClient import ChannelFinderClient
+from Channel import Tag
+from Channel import Channel
 
 if __name__ == '__main__':
-    cf = ChannelFinderClient(BaseURL = 'http://channelfinder.nsls2.bnl.gov:8080/ChannelFinder')
+    cf = ChannelFinderClient(BaseURL = 'http://channelfinder.nsls2.bnl.gov:8080/ChannelFinder', username='boss', password='1234')
+    
+    # add one tag
+    tag = Tag('example1', 'vioc')
+    cf.add(tag=tag)
+    
+    # add a set of tags
+    tags = [Tag('example2', 'vioc'), Tag('example3', 'vioc'), Tag('example4', 'vioc'), Tag('example5', 'vioc')]
+    cf.add(tags=tags)
+    
+    channels = cf.find(name='SR*')
+    chantags = []
+    for channel in channels:
+#        print channel.Name
+        props = channel.getProperties()
+#        tags = channel.getTags()
+        # attach tags to a channel
+        chantags.append(Channel((u'%s' % (channel.Name)), 'vioc', tags=tags))
+        
+#        for k, v in props.items():
+#            print k, v
+#        client.remove(channelName=(u'%s' % channel.Name))
+    cf.add(channels=chantags)
+    
+    # update channels
     channels = cf.find(name='SR*')
     for channel in channels:
         print channel.Name
-        props = channel.getProperties()
-        tags = channel.getTags()
-        for k, v in props.items():
-            print k, v
-#        client.remove(channelName=(u'%s' % channel.Name))
-    print len(channels)
+        # get tags for each channel
+        tmp_tags = channel.getTags()
+        if tmp_tags != None:
+            for tmp_tag in tmp_tags:
+                print tmp_tag
+        
+    # remove one example tags
+    cf.remove(tagName='example1')
+    cf.remove(tagName='example2')
+    cf.remove(tagName='example3')
+    cf.remove(tagName='example4')
+    cf.remove(tagName='example5')
+    
+#    print len(channels)
