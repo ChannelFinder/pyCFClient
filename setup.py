b914a8b234f84090bce1677c38e1fb3982bed505
Guobao Shen <devnull@localhost>
2011-04-04 12:06:27 -0400

    Fix package setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+'''
+Created on Apr 4, 2011
+
+@author: shroffk
+'''
+
+from distutils.core import setup
+
+setup(name='channelfinder',
+      version='1.0',
+      description='Python ChannelFinder Client Lib',
+      author='Kunal Shroff',
+      author_email='shroffk@bnl.gov',
+      packages=['channelfinder', 'channelfinder/lib'],
+     )
