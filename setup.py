a663a2e88c51baee4133f2b62c29a6d0df378819
Guobao Shen <devnull@localhost>
2011-04-07 14:48:12 -0400

    find packages automatically in setup

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ Created on Apr 4, 2011
 @author: shroffk
 '''
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='channelfinder',
       version='1.0',
       description='Python ChannelFinder Client Lib',
       author='Kunal Shroff',
       author_email='shroffk@bnl.gov',
-      packages=['channelfinder', 'channelfinder/lib'],
+      packages = find_packages()
      )
