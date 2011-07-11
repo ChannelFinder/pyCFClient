4d8f66f91b036938b32fa92e702ee7798d784d8d
shroffk <devnull@localhost>
2011-07-11 12:28:07 -0400

    adding bnl license

--- channelfinder/cfUpdate/CFUpdateIOC.py
+++ channelfinder/cfUpdate/CFUpdateIOC.py
@@ -1,4 +1,7 @@
 '''
+Copyright (c) 2010 Brookhaven National Laboratory
+All rights reserved. Use is subject to license terms and conditions.
+
 Created on Apr 1, 2011
 
 @author: shroffk
--- test/ChannelUtilTest.py
+++ test/ChannelUtilTest.py
@@ -1,4 +1,7 @@
 '''
+Copyright (c) 2010 Brookhaven National Laboratory
+All rights reserved. Use is subject to license terms and conditions.
+
 Created on May 10, 2011
 
 @author: shroffk
