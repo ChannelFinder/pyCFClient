613ed81b234ceab26438e410950730d5035acc69
Guobao Shen <devnull@localhost>
2013-04-03 12:31:09 -0400

    revise setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,22 @@ Created on Apr 4, 2011
 @author: shroffk
 '''
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='channelfinder',
-      version='2.0.0',
-      packages=['channelfinder'],
-      description='Python ChannelFinder Client Lib',
-      author='Kunal Shroff',
-      author_email='shroffk@bnl.gov',
-      scripts=['cf-update-ioc', 'cf-monitor-test'],
-      install_requires = ['requests']
-     )
+setup(
+    name='channelfinder',
+    version='2.0.0',
+    description='Python ChannelFinder Client Lib',
+    author='Kunal Shroff',
+    author_email='shroffk@bnl.gov',
+    url='http://channelfinder.sourceforge.net/channelfinderpy',
+    scripts=['cf-update-ioc', 'cf-monitor-test'],
+    packages=['channelfinder', 'channelfinder/util', 'channelfinder/cfUpdate', 'test'],
+      long_description="""\
+      Python ChannelFinder Client Lib
+      """,
+      license='GPL',
+      install_requires=[
+        'requests',
+      ],
+      )
