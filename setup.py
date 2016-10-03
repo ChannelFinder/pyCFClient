85d2bad8763c7f74e75f42388bacbf3834f347fa
Kunal Shroff <shroffk@bnl.gov>
2016-10-03 10:01:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='channelfinder',
-    version='2.0.0',
+    version='3.0.0',
     description='Python ChannelFinder Client Lib',
     author='Kunal Shroff',
     author_email='shroffk@bnl.gov',
